7c11c849e8e2f565d23302460b5b37f281c3d63c
zzzezzz <qorgh3321@naver.com>
2023-12-13 10:03:50 +0900

    Git Test2

--- app/src/main/java/com/example/joosulsa/LoginActivity.java
+++ app/src/main/java/com/example/joosulsa/LoginActivity.java
@@ -10,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        //코드테스트
     }
 }
