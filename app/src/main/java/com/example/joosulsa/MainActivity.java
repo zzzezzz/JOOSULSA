f2c8982719fd6fc9d738f588a370c76d7e2f8f99
jhch1116 <jhch1116@naver.com>
2023-12-13 12:50:07 +0900

    희철

--- app/src/main/java/com/example/joosulsa/MainActivity.java
+++ app/src/main/java/com/example/joosulsa/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         //github 테스트
+        ////sdfsldkjfsldkjflskjdflkj
     }
 }
