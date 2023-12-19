fcc6e85ac3da9ceca056e744b9e982213273c0c1
fdsv8596 <hyunjun01070@gmail.com>
2023-12-19 16:19:08 +0900

    액티비티 1차로 다 만들었음

---
+++ app/src/main/java/com/example/joosulsa/PointHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.joosulsa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PointHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_history);
+    }
+}
