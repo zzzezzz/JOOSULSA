34e2e989efab6e00eb30c160f0e106b0e4de0ed8
fpdls2789 <fpdls2789@gmail.com>
2023-12-21 17:39:53 +0900

    HomeFragment수정중

--- app/src/main/java/com/example/joosulsa/fragment/HomeFragment.java
+++ app/src/main/java/com/example/joosulsa/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,14 +44,32 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
         });
 
+
+        // 객체 생성
+        dataset = new ArrayList<>();
+
+        // 데이터 넣어주기
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test01"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test02"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test03"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test04"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test05"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test06"));
+        dataset.add(new MainCategoryVO(R.drawable.tab1_home,"test07"));
+
+        // 레이아웃 보여주기
+        // manager : 레이아웃 정보를 갖고있음..
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+
+        // 뿌려줄 xml파일에 recycleview 넣어주고 id 값을 꼭 지정해주기
+        // 레이아웃을 설정해주기
+        binding.categoryList.setLayoutManager(manager);
+
+        // 어댑터를 연결해주기
+        adapter = new MainCategoryAdapter(dataset);
+        binding.categoryList.setAdapter(adapter); // setAdapter : 이걸 어댑터에 넣어준다고
+
         return binding.getRoot();
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        dataset = new ArrayList<>();
-
-
-    }
 }
