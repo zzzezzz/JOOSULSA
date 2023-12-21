7d8a1ef9744127053ac2f71cf89c40ac37a6d6f7
fdsv8596 <hyunjun01070@gmail.com>
2023-12-21 17:19:15 +0900

    회원가입 메소드 완성...인가? 나중에 테스트 요망

--- app/src/main/java/com/example/joosulsa/RegisterActivity.java
+++ app/src/main/java/com/example/joosulsa/RegisterActivity.java
@@ -1,15 +1,24 @@
 package com.example.joosulsa;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.joosulsa.databinding.ActivityRegisterBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
@@ -20,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
     private String springUrl = "나중에 스프링 켜지면 여기다 스프링 주소 넣으면 됨";
 
     // 회원가입이라 post로 했는데 아니면 바꾸죠 뭐
-    int getMethod = Request.Method.POST;
+    int postMethod = Request.Method.POST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +50,11 @@ public class RegisterActivity extends AppCompatActivity {
         binding.btnJoin.setOnClickListener(v -> {
             String userName = binding.joinName.getText().toString();
             String userID = binding.joinId.getText().toString();
+            String userPw = binding.joinPw.getText().toString();
+            String userNick = binding.joinNick.getText().toString();
+            String userAddr = binding.joinAddress.getText().toString();
 
+            registerRequest(userName, userID, userPw, userNick, userAddr);
 
         });
 
@@ -49,7 +62,39 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     // 회원가입 데이터 보내는 메소드
-    private void registerRequest(){
+    private void registerRequest(String userName, String userID, String userPw, String userNick, String userAddr){
+        StringRequest request = new StringRequest(
+                postMethod,
+                springUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("LoginActivity", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("LoginActivity", error.toString());
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            // Map<String(key), String(value)>를
+            // getParams를 사용해서 key와 value를 login?key=value 형태로 바꿔준다.
+            protected Map<String, String> getParams() throws AuthFailureError {
+                // 전송방식을 POST로 지정했을 때 사용하는 메소드
+                // 데이터를 전송할 때 Map 형태로 구성하여 리턴해줘야 한다.
+                Map<String, String> params = new HashMap<>();
+                params.put("name", userName);
+                params.put("id", userID);
+                params.put("pw", userPw);
+                params.put("nick", userNick);
+                params.put("address", userAddr);
 
+                return params;
+            }
+        };
     }
 }
