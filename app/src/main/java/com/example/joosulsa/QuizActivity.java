08fb796b21fff2ed130e5cd658eb34b4bc67f19f
fdsv8596 <hyunjun01070@gmail.com>
2023-12-28 15:49:41 +0900

    퀴즈 / 포인트 하는중

--- app/src/main/java/com/example/joosulsa/QuizActivity.java
+++ app/src/main/java/com/example/joosulsa/QuizActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.joosulsa.databinding.ActivityQuizBinding;
@@ -21,6 +22,7 @@ public class QuizActivity extends AppCompatActivity {
         String quizInfo = intent.getStringExtra("quizInfo");
         int quizPoint = intent.getIntExtra("quizPoint", 100);
         int quizNum = intent.getIntExtra("quizNum", 0);
+        Log.d("quizquizquizquiz", quizAnswer);
         binding.titQuiz.setText(quizContent);
 
         String correct = "O";
@@ -29,9 +31,11 @@ public class QuizActivity extends AppCompatActivity {
         // O 버튼을 클릭 시 팝업창 뜨는 코드
         binding.correctBtn.setOnClickListener(v -> {
             if(quizAnswer.equals(correct)){
-                Intent intent1 = new Intent();
+                Intent intent1 = new Intent(getApplicationContext(),QuizPopupActivity.class);
                 intent1.putExtra("quizPoint", quizPoint);
+                intent1.putExtra("quizInfo", quizInfo);
                 intent1.putExtra("quizNum", quizNum);
+                startActivity(intent1);
             }else {
                 Intent intent2 = new Intent();
                 intent2.putExtra("quizInfo", quizInfo);
@@ -41,9 +45,11 @@ public class QuizActivity extends AppCompatActivity {
         // X 버튼을 클릭 시 팝업창 뜨는 코드
         binding.errorBtn.setOnClickListener(v -> {
             if (quizAnswer.equals(wrong)){
-                Intent intent3 = new Intent();
+                Intent intent3 = new Intent(getApplicationContext(),QuizPopupActivity.class);
                 intent3.putExtra("quizPoint", quizPoint);
+                intent3.putExtra("quizInfo", quizInfo);
                 intent3.putExtra("quizNum", quizNum);
+                startActivity(intent3);
             }else {
                 Intent intent4 = new Intent();
                 intent4.putExtra("quizInfo", quizInfo);
