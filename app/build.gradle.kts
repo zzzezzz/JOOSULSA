09ed1e385949f5e26ebeb15cd05e966c9fb7dca8
fpdls2789 <fpdls2789@gmail.com>
2023-12-29 17:50:20 +0900

    카카오맵 작업중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0") // 이미지 동그랗게 만들어주는 라이브러리 사용법 my_page_info
     implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:11.1.0") // 유튜브 화면 뷰
+    // Kakao maps sdk(형식이 바뀜...웹에 있는 방식으로는 안됨.)
+    implementation (files("libs/libDaumMapAndroid.jar"))
 
 }
--- app/src/main/java/com/example/joosulsa/TownRankActivity.java
+++ app/src/main/java/com/example/joosulsa/TownRankActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import com.example.joosulsa.databinding.ActivityTownRankBinding;
 
+import net.daum.mf.map.api.MapView;
+
 public class TownRankActivity extends AppCompatActivity {
+
+
+    private MapView mapView;
+    private ViewGroup mapViewcontainer;
+
     private ActivityTownRankBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        binding = ActivityTownRankBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        binding = ActivityTownRankBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // 지도 띄우기
+        mapView = new MapView(this);
+        mapViewcontainer = (ViewGroup) findViewById(R.id.mapView);
+        mapViewcontainer.addView(mapView);
+
+
+
+
+
     }
 }
