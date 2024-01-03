af727bd9d9966f997fb59844639e5e5f145ba3ce
fpdls2789 <fpdls2789@gmail.com>
2024-01-03 18:35:11 +0900

    카카오맵 api 구현

--- app/src/main/java/com/example/joosulsa/TownRankActivity.java
+++ app/src/main/java/com/example/joosulsa/TownRankActivity.java
@@ -2,19 +2,12 @@ package com.example.joosulsa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.webkit.WebViewClient;
 
 import com.example.joosulsa.databinding.ActivityTownRankBinding;
 
-import net.daum.mf.map.api.MapView;
-
 public class TownRankActivity extends AppCompatActivity {
-
-
-    private MapView mapView;
-    private ViewGroup mapViewcontainer;
 
     private ActivityTownRankBinding binding;
     @Override
@@ -23,13 +16,19 @@ public class TownRankActivity extends AppCompatActivity {
         binding = ActivityTownRankBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // 지도 띄우기
-        mapView = new MapView(this);
-        mapViewcontainer = (ViewGroup) findViewById(R.id.mapView);
-        mapViewcontainer.addView(mapView);
+        // 1. url 값 적어줄 때 카카오 api 연결할 도메인 값도 일치 시킬 것
+        // 2. 로컬 아이피이기 때문에 같은 인터넷을 사용해야해서 연결 시 오류가 날 경우 단말의 인터넷 설정을 확인해보자.(와이파이가 연결되어 있지 않다면 인터넷이 뜨지 않음)
+        String url = "http://192.168.219.42:8089/map"; // defValue : 오류가 나거나 안뜰때 작성해주는 곳
 
+        // 2. WebView를 설정 할 수 있는 객체
+        // 3. WebView의 js 사용을 true로 변경
+        binding.WebView.getSettings().setJavaScriptEnabled(true);
 
+        // 3. WebView의 클라이언트
+        binding.WebView.setWebViewClient(new WebViewClient()); // 내가 갖고 있는 앱으로 띄워주려면 ..
 
+        // 4. url 적용
+        binding.WebView.loadUrl(url);
 
 
     }
