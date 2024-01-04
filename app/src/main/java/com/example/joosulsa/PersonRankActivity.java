1357f2bef9e996be74be210cfd6c98405f18304e
fdsv8596 <hyunjun01070@gmail.com>
2024-01-04 20:00:51 +0900

    dfd

--- app/src/main/java/com/example/joosulsa/PersonRankActivity.java
+++ app/src/main/java/com/example/joosulsa/PersonRankActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -15,7 +18,13 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.joosulsa.databinding.ActivityPersonRankBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PersonRankActivity extends AppCompatActivity {
@@ -54,7 +63,8 @@ public class PersonRankActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-
+                        Log.d("personDataCheck", response);
+                        personDataHandle(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -74,7 +84,59 @@ public class PersonRankActivity extends AppCompatActivity {
         requestQueue.add(request);
     }
 
+    private void personDataHandle(String response) {
 
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+
+            // User ID를 담을 리스트
+            List<String> userNickList = new ArrayList<>();
+
+            // Total Points를 담을 리스트
+            List<Long> totalPointsList = new ArrayList<>();
+
+            // JSON 배열을 순회하며 데이터 추출
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject userData = jsonArray.getJSONObject(i);
+                String userNick = userData.getString("userNick");
+                Long totalPoints = userData.getLong("totalPoints");
+                Log.d("asdcaxczxvqa", userNick);
+                // 추출한 데이터를 리스트에 추가
+                userNickList.add(userNick);
+                totalPointsList.add(totalPoints);
+            }
+
+            // 이제 userIdList와 totalPointsList를 원하는 방식으로 활용할 수 있습니다.
+            for (int i = 0; i < userNickList.size(); i++) {
+                String userNick = userNickList.get(i);
+                Long totalPoints = totalPointsList.get(i);
+                String stringPoints = String.valueOf(totalPoints);
+                Log.d("nghntyb", stringPoints);
+                Log.d("vcvnfgbd", userNick);
+                String tvNickId = "personNick" + (i+1);
+                int nickResourceId = getResources().getIdentifier(tvNickId, "id", getPackageName());
+                Log.d("dasqwdcaqws", Integer.toString(nickResourceId));
+
+                String tvScoreId = "personScore" + (i+1);
+                int scoreResourceId = getResources().getIdentifier(tvScoreId, "id", getPackageName());
+
+                TextView nickTextView = findViewById(nickResourceId);
+                Log.d("asmntrbfg", nickTextView.toString());
+                TextView scoreTextView = findViewById(scoreResourceId);
+                Log.d("asmntrbvx", scoreTextView.toString());
+
+                nickTextView.setText(userNick.toString());
+                scoreTextView.setText(stringPoints);
+
+                // 원하는 로직 수행
+                Log.d("UserRankData", "User ID: " + userNick + ", Total Points: " + totalPoints);
+            }
+
+            // 나머지 로직 계속 작성
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
