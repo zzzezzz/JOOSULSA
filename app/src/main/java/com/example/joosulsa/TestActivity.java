fc8d1bc28136875d4073b41a05600bb396c1d1c1
fpdls2789 <fpdls2789@gmail.com>
2024-01-08 14:33:37 +0900

    동네랭킹

--- app/src/main/java/com/example/joosulsa/TestActivity.java
+++ app/src/main/java/com/example/joosulsa/TestActivity.java
@@ -2,34 +2,30 @@ package com.example.joosulsa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
+
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
+
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.joosulsa.databinding.ActivityTestBinding;
 
 import org.jetbrains.annotations.Nullable;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestActivity extends AppCompatActivity {
     private static final String FLASK_SERVER_URL = "http://192.168.219.51:5000/upload_image";
@@ -39,11 +35,11 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityTestBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        //시발
         Bitmap bitmap = getIntent().getParcelableExtra("TestImg");
-        Log.d("이미지왔어",bitmap.toString());
+        Log.d("확인",bitmap.toString());
         String base64Image = encodeToBase64(bitmap, Bitmap.CompressFormat.PNG,100);
-        Log.d("이미지변환",base64Image.toString());
+        Log.d("변환",base64Image.toString());
         uploadImageToServer(base64Image);
 
     }
@@ -55,25 +51,19 @@ public class TestActivity extends AppCompatActivity {
     }
 
     private void uploadImageToServer(String base64Image){
-        JSONObject jsonBody = new JSONObject();
-        try {
-            jsonBody.put("image",base64Image);
-            Log.d("담았냐?",jsonBody.toString());
-        }catch (JSONException e){
-            e.printStackTrace();
-        }
 
-        JsonObjectRequest request = new JsonObjectRequest(
+
+        StringRequest request = new StringRequest(
                 Request.Method.POST,
                 FLASK_SERVER_URL,
-                jsonBody,
-                new Response.Listener<JSONObject>() {
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(String response) {
                         try {
-                            String result = response.getString("result");
-                            Toast.makeText(TestActivity.this, "서버응답" + result, Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
+                            Log.d("확인11", response.toString());
+
+                            // Toast.makeText(TestActivity.this, "서버응답" +, Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -85,14 +75,17 @@ public class TestActivity extends AppCompatActivity {
                         Toast.makeText(TestActivity.this, "서버오류", Toast.LENGTH_SHORT).show();
                     }
                 }
-        );
+
+        ){
+            @androidx.annotation.Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("image", base64Image);
+                return params;
+            }
+        };
         Volley.newRequestQueue(this).add(request);
     }
-
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/joosulsa/TownRankActivity.java
+++ app/src/main/java/com/example/joosulsa/TownRankActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,10 +19,13 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.joosulsa.databinding.ActivityTownRankBinding;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TownRankActivity extends AppCompatActivity {
@@ -29,9 +33,9 @@ public class TownRankActivity extends AppCompatActivity {
     private ActivityTownRankBinding binding;
 
     // 서버에 요청 보내기 필요함
-    private RequestQueue queue;
+    private RequestQueue requestQueue;
 
-    private String springUrl = "http://172.30.48.1:8089/townList";
+    private String townRankUrl = "http://192.168.219.42:8089/townList";
 
     int postMethod = Request.Method.POST;
 
@@ -41,86 +45,120 @@ public class TownRankActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityTownRankBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+//
+//        // 1. url 값 적어줄 때 카카오 api 연결할 도메인 값도 일치 시킬 것
+//        // 2. 로컬 아이피이기 때문에 같은 인터넷을 사용해야해서 연결 시 오류가 날 경우 단말의 인터넷 설정을 확인해보자.(와이파이가 연결되어 있지 않다면 인터넷이 뜨지 않음)
+//        String url = "http://192.168.219.42:8089/map"; // defValue : 오류가 나거나 안뜰때 작성해주는 곳
+//
+//        // 2. WebView를 설정 할 수 있는 객체
+//        // 3. WebView의 js 사용을 true로 변경
+//        binding.WebView.getSettings().setJavaScriptEnabled(true);
+//
+//        // 3. WebView의 클라이언트
+//        binding.WebView.setWebViewClient(new WebViewClient()); // 내가 갖고 있는 앱으로 띄워주려면 ..
+//
+//        // 4. url 적용
+//        binding.WebView.loadUrl(url);
+//
+//        binding.btnTownRankBack.setOnClickListener(v -> {
+//            Intent intent = new Intent(TownRankActivity.this, MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        });
 
-        // 1. url 값 적어줄 때 카카오 api 연결할 도메인 값도 일치 시킬 것
-        // 2. 로컬 아이피이기 때문에 같은 인터넷을 사용해야해서 연결 시 오류가 날 경우 단말의 인터넷 설정을 확인해보자.(와이파이가 연결되어 있지 않다면 인터넷이 뜨지 않음)
-        String url = "http://192.168.219.42:8089/map"; // defValue : 오류가 나거나 안뜰때 작성해주는 곳
 
-        // 2. WebView를 설정 할 수 있는 객체
-        // 3. WebView의 js 사용을 true로 변경
-        binding.WebView.getSettings().setJavaScriptEnabled(true);
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(TownRankActivity.this);
+        }
 
-        // 3. WebView의 클라이언트
-        binding.WebView.setWebViewClient(new WebViewClient()); // 내가 갖고 있는 앱으로 띄워주려면 ..
+        // 밑에 정의한 메소드
+        townRankData();
 
-        // 4. url 적용
-        binding.WebView.loadUrl(url);
+    }
 
-        binding.btnTownRankBack.setOnClickListener(v -> {
-            Intent intent = new Intent(TownRankActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        });
 
-//
-//        if (queue == null) {
-//            queue = Volley.newRequestQueue(this);
-//        }
-//
-//        String townName = binding.townRank1Nm.toString();
-//        String townPoint = binding.townRank1Score.toString();
-//
-//        // 서버에 데이터 요청
-//        StringRequest request = new StringRequest(
-//                postMethod,
-//                springUrl, new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        handleLoginResponse(response);
-//                    }
-//                }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Log.d("에러","서버에서 값을 받아오지 못했습니다.");
-//                    }
-//                }
-//
-//        ){
-//            @Nullable
-//            @Override
-//            // Map<String(key), String(value)>를
-//            // getParams를 사용해서 key와 value를 login?key=value 형태로 바꿔준다.
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                // 전송방식을 POST로 지정했을 때 사용하는 메소드
-//                // 데이터를 전송할 때 Map 형태로 구성하여 리턴해줘야 한다.
-//                Map<String, String> params = new HashMap<>();
-//                params.put("townName", townName);
-//                params.put("townPoint", townPoint);
-//                Log.d("LoginParams", params.toString());
-//                return params;
-//            }
-//        };
-//        queue.add(request);
-//
-//    }
+    // 동네 랭킹에 사용할 데이터 가져올거
+    private void townRankData(){
 
-//
-//    private void handleLoginResponse(String response) {
-//        // 서버에서 받아온 응답을 처리하는 로직을 작성 : 스프링이 보내준 값을 안드로이드에서 받아주는 작업
-//        // response는 서버에서 보낸 JSON 형태의 데이터일 것이므로, 필요에 따라 파싱하여 사용
-//        try {
-//            JSONObject jsonResponse = new JSONObject(response);
-//            // json 파싱하는 부분
-//            String townName = jsonResponse.getString("townName");
-//            String townPoint = jsonResponse.getString("townPoint");
-//
-//
-//            binding.townRank1Nm.setText(townName);
-//            binding.townRank1Score.setText(townPoint);
-//
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//    }
+        StringRequest request = new StringRequest(
+                postMethod,
+                townRankUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("townDataCheck", response);
+                        townRankDataHandle(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<>();
+                return param;
+            }
+        };
+
+        requestQueue.add(request);
+    }
+
+
+    private void townRankDataHandle(String response) {
+
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+
+            // townName을 담을 리스트
+            List<String> townNameList = new ArrayList<>();
+
+            // Total Points를 담을 리스트
+            List<Long> totalPointsList = new ArrayList<>();
+
+            // JSON 배열을 순회하며 데이터 추출
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject townData = jsonArray.getJSONObject(i);
+                String townName = townData.getString("townName");
+                Long totalPoints = townData.getLong("totalPoints");
+                Log.d("test", townName);
+                // 추출한 데이터를 리스트에 추가
+                townNameList.add(townName);
+                totalPointsList.add(totalPoints);
+            }
+
+            // townNameList와 totalPointsList를 원하는 방식으로 활용
+            for (int i = 0; i < townNameList.size(); i++) {
+                String townName = townNameList.get(i);
+                Long totalPoints = totalPointsList.get(i);
+                String stringPoints = String.valueOf(totalPoints);
+                Log.d("총합 포인트 내역", stringPoints);
+                Log.d("동네 이름", townName);
+                String tvTownName = "townRankNm" + (i+1);
+                int tvTownNameId = getResources().getIdentifier(tvTownName, "id", getPackageName());
+                Log.d("동네 이름 가지고 오나?", Integer.toString(tvTownNameId));
+
+                String tvScoreId = "townRankSc" + (i+1);
+                int scoreResourceId = getResources().getIdentifier(tvScoreId, "id", getPackageName());
+
+                TextView townNameTextView = findViewById(tvTownNameId);
+                Log.d("asmntrbfg", townNameTextView.toString());
+                TextView scoreTextView = findViewById(scoreResourceId);
+                Log.d("asmntrbvx", scoreTextView.toString());
+
+                townNameTextView.setText(townName.toString());
+                scoreTextView.setText(stringPoints);
+
+                // 원하는 로직 수행
+                Log.d("townRankData", "townName: " + townName + ", Total Points: " + totalPoints);
+            }
+
+            // 나머지 로직 계속 작성
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
