eddec286ab2f91c8e66b88d2892dedef444eb8f1
fpdls2789 <fpdls2789@gmail.com>
2024-01-09 10:06:59 +0900

    xml 정리 및 동네랭킹 기능 작업중

--- app/src/main/java/com/example/joosulsa/TownRankActivity.java
+++ app/src/main/java/com/example/joosulsa/TownRankActivity.java
@@ -78,30 +78,28 @@ public class TownRankActivity extends AppCompatActivity {
 
 
 
-//
-//        // 1. url 값 적어줄 때 카카오 api 연결할 도메인 값도 일치 시킬 것
-//        // 2. 로컬 아이피이기 때문에 같은 인터넷을 사용해야해서 연결 시 오류가 날 경우 단말의 인터넷 설정을 확인해보자.(와이파이가 연결되어 있지 않다면 인터넷이 뜨지 않음)
-//        String url = "http://192.168.219.42:8089/map"; // defValue : 오류가 나거나 안뜰때 작성해주는 곳
-//
-//        // 2. WebView를 설정 할 수 있는 객체
-//        // 3. WebView의 js 사용을 true로 변경
-//        binding.WebView.getSettings().setJavaScriptEnabled(true);
-//
-//        // 3. WebView의 클라이언트
-//        binding.WebView.setWebViewClient(new WebViewClient()); // 내가 갖고 있는 앱으로 띄워주려면 ..
-//
-//        // 4. url 적용
-//        binding.WebView.loadUrl(url);
-//
-//        binding.btnTownRankBack.setOnClickListener(v -> {
-//            Intent intent = new Intent(TownRankActivity.this, MainActivity.class);
-//            startActivity(intent);
-//            finish();
-//        });
+
+        // 1. url 값 적어줄 때 카카오 api 연결할 도메인 값도 일치 시킬 것
+        // 2. 로컬 아이피이기 때문에 같은 인터넷을 사용해야해서 연결 시 오류가 날 경우 단말의 인터넷 설정을 확인해보자.(와이파이가 연결되어 있지 않다면 인터넷이 뜨지 않음)
+        String url = "http://192.168.219.42:8089/map"; // defValue : 오류가 나거나 안뜰때 작성해주는 곳
+
+        // 2. WebView를 설정 할 수 있는 객체
+        // 3. WebView의 js 사용을 true로 변경
+        binding.WebView.getSettings().setJavaScriptEnabled(true);
+
+        // 3. WebView의 클라이언트
+        binding.WebView.setWebViewClient(new WebViewClient()); // 내가 갖고 있는 앱으로 띄워주려면 ..
+
+        // 4. url 적용
+        binding.WebView.loadUrl(url);
+
+        binding.btnTownRankBack.setOnClickListener(v -> {
+            Intent intent = new Intent(TownRankActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
 
         // 뒤로 가기 버튼
-
-
 
     }
 
@@ -154,12 +152,9 @@ public class TownRankActivity extends AppCompatActivity {
                 JSONObject townData = jsonArray.getJSONObject(i);
                 String townName = townData.getString("townName");
 
-//                // 한글 인코딩
-//                String jsonString = townData.getString("townName");
-//                byte[] bytes = jsonString.getBytes(StandardCharsets.UTF_8);
-//                String decodedString = new String(bytes, "UTF-8");
+                Log.d("데이터 확인", townName);
+                Log.d("데이터 확인", townData.toString());
 
-//                Log.d("데이터 확인", decodedString);
 
                 Long totalPoints = townData.getLong("totalPoints");
                 Log.d("테스트",  String.valueOf(jsonArray.getJSONObject(i)));
@@ -189,7 +184,7 @@ public class TownRankActivity extends AppCompatActivity {
                 Log.d("asmntrbvx", scoreTextView.toString());
 
                 townNameTextView.setText(townName.toString());
-                scoreTextView.setText(stringPoints);
+                scoreTextView.setText(stringPoints+" pt");
 
                 // 원하는 로직 수행
                 Log.d("townRankData", "townName: " + townName + ", Total Points: " + totalPoints);
